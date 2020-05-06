9c0bb40c13e1672079fab644ae4951afeda3a06f
Titov <titovanton5@mail.ru>
2020-05-06 23:11:58 +0600

    11.3

--- src/main/java/net/thumbtack/school/hiring/exception/ServerException.java
+++ src/main/java/net/thumbtack/school/hiring/exception/ServerException.java
@@ -1,10 +1,9 @@
 package net.thumbtack.school.hiring.exception;
 
 public class ServerException extends Throwable {
-    // !!!
-    //REVU: private - все поля должны быть private
-    ErrorCode errorCode;
-    public ServerException(ErrorCode errorCode){
+    private ErrorCode errorCode;
+
+    public ServerException(ErrorCode errorCode) {
         this.errorCode = errorCode;
     }
 
