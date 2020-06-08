e063186af5e260f2edeae21368e04a459974b663
Titov <titovanton5@mail.ru>
2020-06-08 19:48:31 +0600

    11.4

---
+++ src/test/java/net/thumbtack/school/hiring/server/TestServer.java
@@ -0,0 +1,18 @@
+package net.thumbtack.school.hiring.server;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+
+public class TestServer {
+    @Ignore
+    @Test
+    public void testStartServer(){
+
+    }
+    @Ignore
+    @Test
+    public void testStopServer(){
+
+    }
+
+}
